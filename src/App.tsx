e6e823757c6db8e56a221cc1951b8b3b7ce62018
EugeneY <eugeney@example.com>
2022-01-21 17:15:35 +0200

    css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import {Navbar} from "./components/Navbar";
+import {Menu} from "./components/Menu/Menu";
 import {TodosPage} from "./pages/TodosPage";
 import {AboutPage} from "./pages/AboutPage";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {IconImage} from "./components/IconsGroup/IconImage";
 
 const App: React.FC = () => { //FC - function component
 
-  return (
-          <Router>
-              <Navbar />
-              <div className="container">
-                  <Routes>
-                      <Route path="/" element={<TodosPage />}/>
-                      <Route path="/about" element={<AboutPage />}/>
-                      <Route path="/settings" element={null}/>
-                  </Routes>
-              </div>
-          </Router>
+    return (
+        <Router>
+            <div className="header">
+                <div className={'container'}>
+                    <Menu/>
+                </div>
 
-  );
+            </div>
+            <div className="container">
+                <Routes>
+                    <Route path="/coffee-house" element={<TodosPage/>}/>
+                    <Route path="/coffee" element={<AboutPage/>}/>
+                    <Route path="/your-pleasure" element={null}/>
+                </Routes>
+            </div>
+            <div className="footer">
+                <div className={'container'}>
+                    <Menu/>
+                    <IconImage color={'black'}/>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/IconsGroup/IconImage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './IconImage.css';
+const coffeeIconWhite =  require("../../images/coffee.png")
+const coffeeIconBlack =  require("../../images/cofeeBlack.png")
+
+interface IColorProps {
+    color: string
+}
+
+export const IconImage: React.FC<IColorProps> = (props) => {
+    return (
+         <div className={'icon-block icon-'+ props.color}>
+            {props.color === 'black' && <img alt={'icon'} src={coffeeIconBlack}/>}
+            {props.color === 'white' && <img alt={'icon'} src={coffeeIconWhite}/>}
+        </div>
+    )
+};
