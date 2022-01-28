6052f685bb43180928c1ef5a8d043624b4ddb3a4
EugeneY <eugeney@example.com>
2022-01-28 18:25:25 +0200

    css

--- src/components/CoffeeCard/DescriptionModal.tsx
+++ src/components/CoffeeCard/DescriptionModal.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import {PElement} from "../PElement/PElement"
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import './DescriptionModal.css'
 
 type DescriptionModalType = {
@@ -8,15 +9,30 @@ type DescriptionModalType = {
     controlModal: () => void
 };
 
+
+
 export const DescriptionModal: React.FC<DescriptionModalType> = (props) => {
 
     return (
         <>
-            {props.isVisible &&
+
+                <CSSTransition
+                in={props.isVisible}
+                timeout={3000}
+                unmountOnExit
+                classNames="coffee-card-info-modal"
+            >
+
                 <div className="coffee-card-info-modal active">
-                    <div className={'close-button'} onClick={props.controlModal}><img id="loading" src="http://www.vitorazevedo.net/external_files/loading_small.png"/></div>
+                    <div className={'close-button'} onClick={props.controlModal}>
+                        <img alt={'close'}
+                        src="http://www.vitorazevedo.net/external_files/loading_small.png"/>
+                    </div>
                     <PElement appointment='description'>{props.description}</PElement>
-                </div>}
+                </div>
+            </CSSTransition>
+
+
         </>
     )
 }
